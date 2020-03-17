c1ae6f03e294f16e574c2ac9873822965333f4f4
John Sourvinos <johnsourvinos@hotmail.com>
2020-03-17 10:45:49 +0200

    Adds login action with token creation

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,9 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Users.Controllers {
 
@@ -12,10 +17,12 @@ namespace Users.Controllers {
 
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly AppSettings _appSettings;
 
-        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager) {
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, IOptions<AppSettings> appSettings) {
             _userManager = userManager;
             _signInManager = signInManager;
+            _appSettings = appSettings.Value;
         }
 
         [HttpPost("action")]
@@ -42,6 +49,43 @@ namespace Users.Controllers {
             }
 
             return BadRequest(new JsonResult(errorList));
+
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel formData) {
+
+            var user = await _userManager.FindByNameAsync(formData.Username);
+            var roles = await _userManager.GetRolesAsync(user);
+            var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_appSettings.Secret));
+            var tokenExpiryTime = Convert.ToDouble(_appSettings.ExpireTime);
+
+            if (user != null && await _userManager.CheckPasswordAsync(user, formData.Password)) {
+
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var tokenDescriptor = new SecurityTokenDescriptor {
+                    Subject = new ClaimsIdentity(new Claim[] {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(ClaimTypes.NameIdentifier, user.Id),
+                    new Claim(ClaimTypes.Role, roles.FirstOrDefault()),
+                    new Claim("LoggedOn", DateTime.Now.ToString())
+                    }),
+                    SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature),
+                    Issuer = _appSettings.Site,
+                    Audience = _appSettings.Audience,
+                    Expires = DateTime.UtcNow.AddMinutes(tokenExpiryTime)
+                };
+
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+
+                return Ok(new { token = tokenHandler.WriteToken(token), expiration = token.ValidTo, username = user.UserName });
+
+            }
+
+            ModelState.AddModelError("", "Invalid credentials");
+
+            return Unauthorized(new { LoginError = "Invalid credentials" });
 
         }
 
